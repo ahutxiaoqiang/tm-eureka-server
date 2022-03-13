2063922e2cdcd618876a53a1402553fb668c4d96
ahutxiaoqiang <ahutxiaoqiang@sina.cn>
2022-03-13 13:24:51 +0800

    可运行版本

--- src/main/java/com/wyq/AppApplication.java
+++ src/main/java/com/wyq/AppApplication.java
@@ -3,16 +3,13 @@ package com.wyq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:beanRefContext.xml")
 @EnableEurekaServer
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-        System.out.println("==================AppApplication  start success======================");
     }
     
 }
